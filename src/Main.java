40579686d088ade4cb038ea8cfbae11baa123a75
Gustavo Izaguirre <izaguirreserna@gmail.com>
2026-02-24 11:21:53 +0100

    intentalo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
+        System.out.println("Calculadora lista");
+    }
 
+    public static int sumar(int a, int b) {
+        return a + b;
     }
 }
